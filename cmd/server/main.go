3d63f3d0297093bd20b35fecda97b38e97a1e987
sebmaz <seb.maz93@icloud.com>
2025-12-17 22:24:21 +0400

    Add rabbitmq stomp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -11,7 +17,13 @@ const (
 func main() {
 	conn, err := amqp.Dial(rmqURL)
 	if err != nil {
-
+		log.Fatalf("couldn't start RMQ connection: %v", err)
 	}
 	defer conn.Close()
+	log.Println("connection successfull")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+	fmt.Println("the program is shutting down")
 }
