6e8330d816c8744c8f1dae3889bb2ce5187fc818
sebmaz <seb.maz93@icloud.com>
2025-12-18 22:34:22 +0400

    add client conn

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	rmqConnStr = "amqp://guest:guest@localhost:5672/"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	conn, err := amqp.Dial(rmqConnStr)
+	if err != nil {
+		log.Fatalf("cannot open RMQ connection: %v", err)
+	}
+	defer conn.Close()
+	log.Print("connected to RMQ")
+	wlcmMsg, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Printf("error welcoming: %v", err)
+	}
+	log.Print(wlcmMsg)
 }
